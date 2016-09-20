f627105fa63fd5fdb5ae2b6357acae668ea0623f
Matt Pinner <matthewx.pinner@intel.com>
2016-09-20 20:59:02 +0100

    ble running in start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,16 +1,29 @@
-#/bin/bash
+#!/bin/bash
+
 
 # save old display for fixing later
 OLD_DISPLAY=$DISPLAY
 export DISPLAY=:0
 
+
 #kill old start.sh that might be running
+echo "killing any python running..."
+killall python
+
+echo "running curie ble watcher"
+#./run-curie-ble.sh &> /tmp/intel-ble.log &
+
+
+#kill old start.sh that might be running
+echo "killing any java running..."
 killall java
 
+#zone setting
+echo "ZONE is set to $ZONE"
 
-# default 
+# default
 START_SKETCH=Pfw
-if [ $# -eq 1 ] 
+if [ $# -eq 1 ]
     then
     START_SKETCH=$1
 fi
@@ -21,7 +34,7 @@ SKETCH_DIR=/home/intel/Visuals/
 #SKETCH_DIR=/media/intel/3662-3362/Visuals
 
 echo "from $SKETCH_DIR/$START_SKETCH/"
- 
+
 cd /home/intel/processing-3.1.1
 
 ./processing-java --sketch=$SKETCH_DIR/$START_SKETCH/ --present &> /tmp/intel.log
