62f4a89b4a2aa8123ed7d76ca1e5433ff5832dac
Matt Pinner <matthewx.pinner@intel.com>
2016-09-21 23:30:46 +0100

    ble curie restarts and device id changes

--- bin/run-curie-ble.sh
+++ bin/run-curie-ble.sh
@@ -1,21 +1,22 @@
 DEVICE_ID_0="84:68:3E:00:F3:AC"
-DEVICE_ID_1="84:68:3E:00:F3:A2"
-DEVICE_ID_2="84:68:3E:00:F3:C2"
+DEVICE_ID_1="84:68:3E:00:F3:BF"
+#DEVICE_ID_2="84:68:3E:00:F3:C2"
 
 EXEC="python curie_ble.py -d"
 FAKE="python fake_ble_data.py"
 
 while true; do
+    echo "Running '$EXEC $DEVICE_ID_0' ..."
     $EXEC $DEVICE_ID_0
-    echo "Server '$EXEC $DEVICE_ID_0' crashed with exit code $?.  Respawning.." >&2
-    sleep 1
+    echo "Server '$EXEC $DEVICE_ID_0' exited with exit code $?."
+    echo "Running '$EXEC $DEVICE_ID_1' ..."
     $EXEC $DEVICE_ID_1
-    echo "Server '$EXEC $DEVICE_ID_1' crashed with exit code $?.  Respawning.." >&2
-    sleep 1
+    echo "Server '$EXEC $DEVICE_ID_1' exited with exit code $?."
+    echo "Running '$EXEC $DEVICE_ID_2' ..."
     $EXEC $DEVICE_ID_2
-    echo "Server '$EXEC $DEVICE_ID_2' crashed with exit code $?.  Respawning.." >&2
-    sleep 1
+    echo "Server '$EXEC $DEVICE_ID_2' exited with exit code $?."
+    echo "Running '$FAKE' ..."
     $FAKE
-    echo "Server '$FAKE' crashed with exit code $?.  Respawning.." >&2
+    echo "Server '$FAKE' done with code $?."
     sleep 1
 done
