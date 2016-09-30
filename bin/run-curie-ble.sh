14f4655251ccdd815c6c3ddb856acb9ab4b39247
Matt Pinner <matthewx.pinner@intel.com>
2016-09-30 02:18:46 +0200

    new rules of the game: glasses on first

--- bin/run-curie-ble.sh
+++ bin/run-curie-ble.sh
@@ -3,27 +3,20 @@
 #DEVICE_ID_2="84:68:3E:00:F3:C2"
 
 EXEC="python ble_autostart_collector.py -d"
-#RECORDED_DATA="python recorded_ble_data.py"
-RECORDED_DATA="echo noop"
+RECORDED_DATA="python recorded_ble_data.py"
+#RECORDED_DATA="echo noop"
 
 while true; do
-    echo "Running '$EXEC $DEVICE_ID_0' ..."
-    $EXEC $DEVICE_ID_0
-    echo "Server '$EXEC $DEVICE_ID_0' exited with exit code $?."
-    echo "Running '$EXEC $DEVICE_ID_1' ..."
-    $EXEC $DEVICE_ID_1
-    echo "Server '$EXEC $DEVICE_ID_1' exited with exit code $?."
-    echo "Running '$EXEC $DEVICE_ID_2' ..."
-    $EXEC $DEVICE_ID_2
-    echo "Server '$EXEC $DEVICE_ID_2' exited with exit code $?."
-    echo "Running '$RECORDED_DATA' once ..."
-    $RECORDED_DATA
-    echo "Server '$RECORDED_DATA' done with code $?."
-    echo "Running '$RECORDED_DATA' twice ..."
-    $RECORDED_DATA
-    echo "Server '$RECORDED_DATA' done with code $?."
-    echo "Running '$RECORDED_DATA' thrice ..."
-    $RECORDED_DATA
-    echo "Server '$RECORDED_DATA' done with code $?."
-    sleep 1
+  echo "Running '$EXEC $DEVICE_ID_0' ..."
+  $EXEC $DEVICE_ID_0
+  echo "Running '$EXEC $DEVICE_ID_0' ..."
+  sleep 1
+  $EXEC $DEVICE_ID_0
+  echo "Server '$EXEC $DEVICE_ID_0' exited with exit code $?."
+    while true; do
+      echo "Running '$RECORDED_DATA' once ..."
+      $RECORDED_DATA
+      echo "Server '$RECORDED_DATA' done with code $?."
+      sleep 1
+    done
 done
